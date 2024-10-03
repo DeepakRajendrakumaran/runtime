ad4b87772b9930a3f3261082ac41d4df222fbe28
Deepak Rajendrakumaran <deepak.rajendrakumaran@intel.com>
2024-10-03 09:55:17 -0700

    Updating branch to use correct method.

--- src/coreclr/jit/compiler.cpp
+++ src/coreclr/jit/compiler.cpp
@@ -3405,7 +3405,7 @@ void Compiler::compInitOptions(JitFlags* jitFlags)
 
     // TODO-Xarch-apx:
     // Eventually this should be just isApxSupported
-    if (canUseRex2Encoding())
+    if (canUseApxEncodings())
     {
         rbmAllInt |= RBM_HIGHINT;
         rbmIntCalleeTrash |= RBM_HIGHINT;
