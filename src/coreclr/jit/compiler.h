6ec6979308344b156c34095251a1e379d9dba008
Deepak Rajendrakumaran <deepak.rajendrakumaran@intel.com>
2025-01-16 15:35:06 -0800

    Revert "Updating available in regs"
    This reverts commit d7a769d23267b852332e46577e7f17e4d2fe68ae.

--- src/coreclr/jit/compiler.h
+++ src/coreclr/jit/compiler.h
@@ -11747,7 +11747,6 @@ private:
     regMaskTP rbmAllInt;
     regMaskTP rbmIntCalleeTrash;
     unsigned  cntCalleeTrashInt;
-    regNumber regIntLast;
 public:
     FORCEINLINE regMaskTP get_RBM_ALLFLOAT() const
     {
@@ -11773,10 +11772,6 @@ public:
     FORCEINLINE unsigned get_CNT_CALLEE_TRASH_INT() const
     {
         return this->cntCalleeTrashInt;
-    }
-    FORCEINLINE regNumber get_REG_INT_LAST() const
-    {
-        return this->regIntLast;
     }
 
 #endif // TARGET_AMD64
