3fd5b719f9a1ae73762cee80791d911223c45ac4
Ruihan-Yin <ruihan.yin@intel.com>
2025-03-19 13:47:50 -0700

    Add JitBypassApxCheck knob

--- src/coreclr/jit/compiler.h
+++ src/coreclr/jit/compiler.h
@@ -9870,6 +9870,12 @@ public:
     //
     bool canUseApxEncoding() const
     {
+#ifdef DEBUG
+        if (JitConfig.JitBypassApxCheck())
+        {
+            return true;
+        }
+#endif
         return compOpportunisticallyDependsOn(InstructionSet_APX);
     }
 
